d47484b13e9191ce026c54c5e08b3f17194f1357
Andrea Damiani <andrea.dam90@gmail.com>
2023-01-03 14:53:16 +0100

    Utilizzato il submit tramite form come metodo d'inserimento della città
    A seguito di questa modifica è stato pulito il codice javascript

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-// Pulsanti
-let invia = document.querySelector('#button');
-let reset = document.querySelector('#reset');
-// Campo di input
-let input = document.querySelector("#city");
+let form = document.querySelector('#form'); // Form d'inserimento città
+
+let reset = document.querySelector('#reset'); // Pulsante reset
+
+let input = document.querySelector("#city"); // Campo di input
 // Alerts di errore
 let avvertimento = document.querySelector('#avvertimento');
 let errore = document.querySelector('#errore');
-// Sezione principale che appare quando si fa una ricerca
-let pagina = document.querySelector('#pagina');
-// Icona meteo
-let weatherIcon = document.querySelector('#icona-meteo');
 
-function getWeather () {
+let pagina = document.querySelector('#pagina'); // Sezione principale che appare quando si fa una ricerca
+
+let weatherIcon = document.querySelector('#icona-meteo'); // Icona meteo
+
+function getWeather() {
     
-    let cityName = input.value;
+    reset.classList.remove('d-none'); // Fa apparire il pulsante "reset"
+    
+    input.blur(); // Toglie il focus dal campo input
+
+    let cityName = input.value; // Valore inserito nel campo input
     
     if (cityName == 0) {
         input.classList.add("is-invalid");
@@ -24,39 +28,24 @@ function getWeather () {
         errore.classList.add('d-none');
         
         fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f49f974ab3536d74d780021f75e07be5`).then((response) => response.json()).then((data) => {
-
             
-            // console.log(data.list);
+            let info = data.list; // Lista completa delle informazioni
             
-            // Lista completa delle informazioni
-            let info = data.list;
-            
-            // Se abbiamo scritto male la città
-            if (info == undefined) {
+            if (info == undefined) { // Se abbiamo scritto male la città
                 errore.classList.remove('d-none');
             }
-            // Prendiamo solo il primo elemento (informazioni meteo attuali)
-            let currentWeather = info[0];
+            
+            let currentWeather = info[0]; // Prendiamo solo il primo elemento (informazioni meteo attuali)
             
             // Prendiamo solo il meteo e la temperatura
             let weather = currentWeather.weather;
-            
             let temp = currentWeather.main;
             
-            // console.log(temp.temp);
+            let tempCelsius = (temp.temp - 273.15).toFixed(1); // Convertiamo la temperatura in Celsius e la arrotondiamo alla prima cifra decimale
+
+            document.querySelector('#nomeCittà').innerHTML = cityName.charAt(0).toUpperCase() + cityName.slice(1); // Stampare la città digitata con la prima lettera maiuscola
             
-            // Convertiamo la temperatura in Celsius e la arrotondiamo alla prima cifra decimale
-            let tempCelsius = (temp.temp - 273.15).toFixed(1);
-            
-            // console.log(currentWeather.weather);
-            
-            // console.log(tempCelsius);
-            
-            
-            // Stampare la città digitata con la prima lettera maiuscola
-            document.querySelector('#nomeCittà').innerHTML = cityName.charAt(0).toUpperCase() + cityName.slice(1);
-            
-            // Stampare il meteo
+            // Stampare il meteo e tradurlo in italiano
             if (weather[0].main == "Clouds") {
                 document.querySelector('#meteo').innerHTML = "Nuvoloso";
                 weatherIcon.classList.add('fa-cloud-sun');
@@ -68,29 +57,18 @@ function getWeather () {
                 weatherIcon.classList.add('fa-cloud-rain');
             };
             
-            // Stampare la temperatura
-            document.querySelector('#temp').innerHTML = tempCelsius + " °C";
+            document.querySelector('#temp').innerHTML = tempCelsius + " °C"; // Stampa la temperatura
 
-            // Rendere visibili le informazioni richieste
-            pagina.classList.remove('d-none');
+            pagina.classList.remove('d-none'); // Rende visibili le informazioni richieste
         });
     }
 };
 
-// Pulsante "Invia"
-invia.addEventListener('click', () => {
-    reset.classList.remove('d-none');
+// Invio delle informazioni
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
     getWeather();
-});
-
-// Quando si preme "Invio" sulla tastiera
-input.addEventListener('keyup', (event) => {
-    if (event.keyCode === 13) {
-        reset.classList.remove('d-none');
-        input.blur();
-        getWeather();
-    }
-});
+})
 
 // Pulsante "Reset"
 reset.addEventListener('click', () => {
